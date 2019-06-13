7fff998153e565a0bd3ca2cca8fce9717428a995
zhuyuan <15291990201@163.com>
2019-06-13 14:05:52 +0800

    init

---
+++ 小程序生成条形码和二维码/qrcode/qrcode.js
@@ -0,0 +1,69 @@
+// pages/qrcode/qrcode.js
+var wxbarcode = require('../../utils/index.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //canvas的id，字符串，宽，高
+    wxbarcode.barcode('jsBarcode', '1234567890123456789', 574, 146);//条形码
+    wxbarcode.qrcode('canvas', '1234567890123456789', 340, 340);//二维码
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ 自定义顶部导航栏/text/text.js
@@ -0,0 +1,75 @@
+// pages/text/text.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    current: 0,
+  },
+  //顶部导航栏点击切换事件
+  switchs(e) {
+    let that = this
+    let index = e.currentTarget.dataset.item
+    if (that.data.current != index) {
+      that.setData({
+        current: index
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
